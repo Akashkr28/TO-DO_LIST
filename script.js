24d6f29ba576387721e9a7ab4bbab93a51dbf832
Akashkr28 <email.akash2010@gmail.com>
2024-05-09 21:18:26 +0530

    project completed

--- script.js
+++ script.js
@@ -13,14 +13,25 @@ function addTask(){
             span.innerHTML = "\u00d7";                      // set the text inside of <span>
             li.appendChild(span);                           // append <span> to <li>
     }
-    inputBox.value ="";                                     // Clear the box for next entry
+    inputBox.value = "";                                     // Clear the box for next entry
+    saveData();
 }
 
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");       // Change class of clicked item to 'checked' or remove it
+        saveData()
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        saveData();
     }
 }, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
