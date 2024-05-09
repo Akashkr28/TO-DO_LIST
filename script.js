6a7e17d93bfd62c757b68b804de6cb14924a317b
Akashkr28 <email.akash2010@gmail.com>
2024-05-09 17:33:18 +0530

    Todo App JS

---
+++ script.js
@@ -0,0 +1,26 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+            alert("Please enter a task!");
+        }
+        else{
+            let li = document.createElement("li");          // Create <li>
+            li.innerHTML = inputBox.value;                  // Set text of <li> to input
+            listContainer.appendChild(li);                  // Append <li> to <ul> with id="list-container"
+            let span = document.createElement("span");      // create <span> element
+            span.innerHTML = "\u00d7";                      // set the text inside of <span>
+            li.appendChild(span);                           // append <span> to <li>
+    }
+    inputBox.value ="";                                     // Clear the box for next entry
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");       // Change class of clicked item to 'checked' or remove it
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
